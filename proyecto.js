0f74419c85470bdf65f23f4b819d1d2571658ed9
Anayte Esquivel <74271942+anayte@users.noreply.github.com>
2021-05-09 23:57:29 -0600

    v4

--- proyecto.js
+++ proyecto.js
@@ -1,31 +1,8 @@
-let http = require('http');
-let indexFile;
-const fs = require('fs').promises;
-const express = require("express");
-const router = express.Router();
+const express = require('express');
+const app = express();
 
-const host = 'localhost';
-const port = 3000;
+app.use(express.static(__dirname + '/public/'));
 
-const requestListener = function (req, res) {
-    res.setHeader("Content-Type", "text/html");
-    res.writeHead(200);
-    res.end(indexFile);
-};
-
-router.get("/", (req,res)=>{
-    res.render("/index.html");
+app.listen('3000', function() {
+  console.log('Server is running on http://localhost:3000');
 });
-
-const server = http.createServer(requestListener);
-fs.readFile(__dirname + "/index.html")
-    .then(contents => {
-        indexFile = contents;
-        server.listen(port, host, () => {
-            console.log('Server is running on http://localhost:3000');
-        });
-    })
-    .catch(err => {
-        console.error('Could not read index.html file:', {err});
-        process.exit(1);
-    });
