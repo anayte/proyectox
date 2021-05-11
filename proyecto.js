b980c5822166df7d3039e1fa639bf4eb2a094bf9
Anayte Esquivel <74271942+anayte@users.noreply.github.com>
2021-05-11 02:38:37 -0600

    v5

--- proyecto.js
+++ proyecto.js
@@ -1,8 +1,11 @@
+//Exporta las librerias 
 const express = require('express');
 const app = express();
 
+//llama la libreria para registrar la ruta de los archivos 
 app.use(express.static(__dirname + '/public/'));
 
+//levanta el servicio en el puerto indicado 
 app.listen('3000', function() {
   console.log('Server is running on http://localhost:3000');
 });
